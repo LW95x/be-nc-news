b14870661d203760b87c32f6f483fe1905338d62
LW95x <liam.woodall@live.co.uk>
2023-11-20 15:38:45 +0000

    completed task 3

--- db/app.js
+++ db/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { getAllTopics } = require("./controllers/getTopics.controller");
+const { getApi } = require("./controllers/getApi.controller");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/api/topics", getAllTopics);
+
+app.get("/api", getApi);
 
 module.exports = app;
