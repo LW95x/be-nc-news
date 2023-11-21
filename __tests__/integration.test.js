0dcb1d54cc4897751a8f58f5197b5c0490720ef7
LW95x <liam.woodall@live.co.uk>
2023-11-21 14:49:01 +0000

    get-articles 2

--- __tests__/integration.test.js
+++ __tests__/integration.test.js
@@ -2,6 +2,7 @@ const express = require("express");
 const request = require("supertest");
 const db = require("../db/connection");
 const seed = require("../db/seeds/seed");
+require("jest-sorted");
 const {
   articleData,
   commentData,
@@ -9,6 +10,7 @@ const {
   userData,
 } = require("../db/data/test-data/index.js");
 const app = require("../db/app");
+const { expect } = require("@jest/globals");
 
 beforeEach(() => {
   return seed({ articleData, commentData, topicData, userData });
@@ -99,11 +101,17 @@ describe("GET /api/articles", () => {
   test("responds with a 200 status code", () => {
     return request(app).get("/api/articles").expect(200);
   });
-  test("200: responds with an articles array of objects with the correct properties", () => {
+  test("200: responds with an articles array of objects with the correct properties sorted in descending order with a comment_count", () => {
     return request(app)
       .get("/api/articles")
       .expect(200)
       .then(({ body }) => {
+        console.log(body.articles);
+        expect(body.articles.length).toBe(13);
+        expect(body.articles).toBeSorted({
+          descending: true,
+          key: "created_at",
+        });
         body.articles.forEach((property) => {
           expect(typeof property.article_id).toBe("number");
           expect(typeof property.title).toBe("string");
@@ -113,26 +121,8 @@ describe("GET /api/articles", () => {
           expect(typeof property.votes).toBe("number");
           expect(typeof property.article_img_url).toBe("string");
           expect(typeof property.comment_count).toBe("string");
-        });
-      });
-  });
-  test("200: should respond with an array of articles without a body property present on any of the objects", () => {
-    return request(app)
-      .get("/api/articles")
-      .expect(200)
-      .then(({ body }) => {
-        body.articles.forEach((property) => {
           expect(property.body).toBeUndefined();
         });
       });
   });
-  test.todo(
-    "200: should return an array of objects in descending order based on created_at unix code"
-  );
 });
-
-// Any pointers on what else I can test for here? Also, how can I test that these are sorted by descending order when the value is in a strange format, i.e. "2020-11-03T09:12:00.000Z"
-
-// It seems very convoluted to me to have to convert each one to a date to be able to compare with the next by order. Is there something simple I'm missing?
-
-// Also, should comment_count be incrementing? What is a comment defined as? A little confused there. I'm assuming 1 body = 1 comment.
--- db/models/getArticles.model.js
+++ db/models/getArticles.model.js
@@ -1,9 +1,22 @@
 const db = require("../connection");
 
 exports.findArticles = () => {
-    return db.query(`SELECT article_id, title, topic, author, created_at, votes, article_img_url, COUNT(body) AS comment_count FROM articles 
-    GROUP BY article_id, title, topic, author, created_at, votes, article_img_url
-    ORDER BY created_at DESC`).then(({rows}) => {
-        return rows;
-    })
-}
+  return db
+    .query(
+      `SELECT 
+    a.article_id, 
+    a.title, topic, 
+    a.author, 
+    a.created_at, 
+    a.votes, 
+    a.article_img_url, 
+    COUNT(c.comment_id) AS comment_count 
+    FROM articles a
+    LEFT JOIN comments c ON a.article_id = c.article_id
+    GROUP BY a.article_id, a.title, a.topic, a.author, a.created_at, a.votes, a.article_img_url
+    ORDER BY a.created_at DESC`
+    )
+    .then(({ rows }) => {
+      return rows;
+    });
+};
