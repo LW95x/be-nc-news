640cd3e4287fcd4d6dc15a32608854ea2fc3a8fb
LW95x <liam.woodall@live.co.uk>
2023-11-24 09:11:39 +0000

    delete-comment-by-comment-id

---
+++ db/controllers/deleteCommentById.controller.js
@@ -0,0 +1,13 @@
+const { removeCommentById } = require("../models/deleteCommentById.model");
+
+exports.deleteCommentById = (req, res, next) => {
+    const id = req.params.comment_id;
+    
+    removeCommentById(id)
+    .then( (comment) => {
+        res.status(204).send(comment);
+    })
+    .catch((err) => {
+        next(err);
+    })
+}
