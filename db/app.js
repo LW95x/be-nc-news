b4b4e2d89df3594be71cc02788ef959d222efde8
LW95x <liam.woodall@live.co.uk>
2023-12-11 10:22:16 +0000

    allow cross origin resource sharing

--- db/app.js
+++ db/app.js
@@ -19,9 +19,9 @@ const {
 const { getUsers } = require("./controllers/getUsers.controller");
 const cors = require ('cors');
 
-app.use(cors());
-
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
