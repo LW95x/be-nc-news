181902e168dce0a23293bed45e266048dfd86e07
LW95x <142240173+LW95x@users.noreply.github.com>
2025-02-18 23:36:36 +0000

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -10,15 +10,12 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-const config = {
-  ssl: {
-    rejectUnauthorized: false,
-  },
-};
+const config = {};
 
 if (ENV === 'production') {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+  config.ssl = { rejectUnauthorized: false }; 
 }
 
 module.exports = new Pool(config);
